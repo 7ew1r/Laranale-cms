4777de28f1d5bca2e2382d25e1603f869995a24e
7ew1r <art.sht.0317@gmail.com>
2019-05-28 14:09:09 +0900

    Update: add link to post edit page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,15 +5,16 @@
 
 @foreach($posts as $post)
 
-	<h2>{{ link_to("posts/{$post->id}" ,$post->title ) }}
-		<small>投稿日：{{ date("Y年 m月 d日",strtotime($post->created_at)) }}</small>
-	</h2>
+	<h2>{{ link_to("posts/{$post->id}" ,$post->title ) }}</h2>
 	@php
 			$user_id = DB::table('users')->where('id', $post->user_id)->value('id');
 			$user_name = DB::table('users')->where('id', $post->user_id)->value('name');
 	@endphp
-	<p>by {{ link_to("users/{$user_id}" ,$user_name) }}
-	<p>コメント：{{ $post->comment_count }}</p>
+	<p>
+	<span>by {{ link_to("users/{$user_id}" ,$user_name) }}</span>
+	<span><i class="fas fa-comment"></i> {{ $post->comment_count }}</span>
+	<span>投稿日：{{ date("Y年 m月 d日",strtotime($post->created_at)) }}</span>
+	</p>
 	<hr />
 @endforeach
 
