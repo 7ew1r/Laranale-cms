9790c54195402779e44e258da0d7c54bf2e82a19
7ew1r <art.sht.0317@gmail.com>
2019-06-04 20:07:00 +0900

    Add: add footer

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -80,6 +80,9 @@
 
   @yield('content')
 
+  <footer class="footer">
+    <p>&copy; 2019 TEWi_R</p>
+  </footer>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
   </script>
