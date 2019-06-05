6a40803cfe5f99e1c19be1a275964f9951152e6a
7ew1r <art.sht.0317@gmail.com>
2019-06-05 15:29:15 +0900

    Layout: tweak layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container">
   <div class="row">
-    <div class="col-md-8 offset-2">
+    <div class="col-md-8 offset-2 my-5">
       <div class="card">
         <div class="card-header">ログイン</div>
 
@@ -13,9 +13,9 @@
             {{ csrf_field() }}
 
             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-              <label for="email" class="col-md-4 control-label">メールアドレス</label>
+              <label for="email" class="control-label">メールアドレス</label>
 
-              <div class="col-md-6">
+              <div class="">
                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required
                   autofocus>
 
@@ -28,9 +28,9 @@
             </div>
 
             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-              <label for="password" class="col-md-4 control-label">パスワード</label>
+              <label for="password" class="control-label">パスワード</label>
 
-              <div class="col-md-6">
+              <div class="">
                 <input id="password" type="password" class="form-control" name="password" required>
 
                 @if ($errors->has('password'))
@@ -42,7 +42,7 @@
             </div>
 
             <div class="form-group">
-              <div class="col-md-6 col-md-offset-4">
+              <div class="">
                 <div class="checkbox">
                   <label>
                     <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
@@ -52,7 +52,7 @@
             </div>
 
             <div class="form-group">
-              <div class="col-md-8 col-md-offset-4">
+              <div class="">
                 <button type="submit" class="btn btn-primary">
                   ログイン
                 </button>
